586e8bfebf73ff00fd992f991ae5fdb6a241ffd1
gurwinderdhaliwal4785 <133153247+gurwinderdhaliwal4785@users.noreply.github.com>
2023-10-09 10:22:09 +0530

    Add files via upload

---
+++ FooterDevDeakin.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './FooterBanner.css'
+
+
+function FooterBanner() {
+
+    return(
+        <div class="footer-dark">
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div className="dev">
+                            <p>DEV@Deakin 2023</p>
+                            <div className="dev2">
+                            <p>Privacy Policy</p>
+                            <p>Terms</p>
+                            <p>Code Of Conduct</p>
+                            </div>
+                         </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+    )
+}
+
+export default FooterBanner
